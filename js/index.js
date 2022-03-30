a62eecf27aa6b2f1f340b0f53e91024929db309d
atenobi <atenobimailru@gmail.com>
2022-03-31 03:32:05 +0400

    fix responsive desktop/tablet

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 const burgerButtonEl = document.getElementById('js-responsive-menu-button');
 const mobileMenuEl = document.getElementById('js-nav-menu');
 
-const showMenuFunc = () => {
-    mobileMenuEl.classList.toggle('mobile-menu');  
-};
+const showMenuFunc = () => mobileMenuEl.classList.toggle('mobile-menu');  
 
 burgerButtonEl.addEventListener('click', showMenuFunc);
